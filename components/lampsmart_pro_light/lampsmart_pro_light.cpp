87688f5ae452ced7288526bffe4ac98151abafe6
Lev Aronsky <aronsky@gmail.com>
2023-05-27 11:49:02 +0300

    Fix format string

--- components/lampsmart_pro_light/lampsmart_pro_light.cpp
+++ components/lampsmart_pro_light/lampsmart_pro_light.cpp
@@ -137,8 +137,8 @@ void LampSmartProLight::write_state(light::LightState *state) {
 
 void LampSmartProLight::dump_config() {
   ESP_LOGCONFIG(TAG, "LampSmartProLight '%s'", light_state_ ? light_state_->get_object_id().c_str() : "");
-  ESP_LOGCONFIG(TAG, "  Cold White Temperature: %d mireds", cold_white_temperature_);
-  ESP_LOGCONFIG(TAG, "  Warm White Temperature: %d mireds", warm_white_temperature_);
+  ESP_LOGCONFIG(TAG, "  Cold White Temperature: %f mireds", cold_white_temperature_);
+  ESP_LOGCONFIG(TAG, "  Warm White Temperature: %f mireds", warm_white_temperature_);
   ESP_LOGCONFIG(TAG, "  Constant Brightness: %s", constant_brightness_ ? "true" : "false");
   ESP_LOGCONFIG(TAG, "  Minimum Brightness: %d", min_brightness_);
   ESP_LOGCONFIG(TAG, "  Transmission Duratoin: %d millis", tx_duration_);
