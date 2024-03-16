c55747c60789f7db457c54d573b241fe98c180ba
Lev Aronsky <aronsky@gmail.com>
2024-03-16 09:23:46 +0200

    Handle NULL args

--- components/ble_adv_light/zhijia_light.cpp
+++ components/ble_adv_light/zhijia_light.cpp
@@ -64,7 +64,7 @@ void ZhiJiaLight::write_state(light::LightState *state) {
 }
 
 void ZhiJiaLight::send_packet(uint8_t cmd, uint8_t *val) {
-  unsigned char args[] = {val[0], 0, 0};
+  unsigned char args[] = {val ? val[0] : 0, 0, 0};
   unsigned char mfg_data[0x1a] = {0};
   char mfg_data_dump[3 * sizeof(mfg_data) + 1] = {0};
   esp_ble_adv_data_t adv_data = {
